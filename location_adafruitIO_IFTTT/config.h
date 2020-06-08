202158ac9064754a433aa44990d9c0cc6852be15
Iván Sánchez Milara <ivanmilara@users.noreply.github.com>
2020-06-08 17:32:06 +0300

    Update config.h

--- location_adafruitIO_IFTTT/config.h
+++ location_adafruitIO_IFTTT/config.h
@@ -2,8 +2,8 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME "ivanmilara"
-#define IO_KEY "aio_DmRd45XW3N79sE9aDWzJmff9wpgm"
+#define IO_USERNAME "adafruitIO_username"
+#define IO_KEY "adafruit_io_key"
 
 /******************************* WIFI **************************************/
 
@@ -20,8 +20,8 @@
 //   - Adafruit AirLift Shield -> https://www.adafruit.com/product/4285
 //   - Adafruit AirLift FeatherWing -> https://www.adafruit.com/product/4264
 
-#define WIFI_SSID "invitado_kalevala"
-#define WIFI_PASS "kulervohijodekalervo"
+#define WIFI_SSID "yourwifi_SSID"
+#define WIFI_PASS "yourwifi_pass"
 
 // uncomment the following line if you are using airlift
 // #define USE_AIRLIFT
